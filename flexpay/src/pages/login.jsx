3997fa16d5add50d042faed1b8b1a39032d76f61
Mukta singh <muktasingh615@gmail.com>
2025-10-10 00:17:24 +0530

    login page

--- flexpay/src/pages/login.jsx
+++ flexpay/src/pages/login.jsx
@@ -52,9 +52,9 @@ function Login() {
   // <html className="h-full bg-white">
   // <body className="h-full">
   // ```
-<div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+<div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8 mt-5 ">
   <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-    <img src={LOGO01} alt="flexpay" className="mx-auto h-8 w-10 w-auto" />
+    <img src={LOGO01} alt="flexpay" className="mx-auto h-6 w-10 w-auto" />
     <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Sign in to your account</h2>
   </div>
 
